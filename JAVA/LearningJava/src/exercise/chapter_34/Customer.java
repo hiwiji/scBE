3915737028e57749bd976b994cb399e66008b01c
jieun wi <jjewwi12@gmail.com>
2024-10-22 05:13:59 +0900

    241021 week4 day1 java study

---
+++ JAVA/LearningJava/src/exercise/chapter_34/Customer.java
@@ -0,0 +1,40 @@
+package exercise.chapter_34;
+
+public class Customer {
+
+    // 속성
+    // 고객ID, 고객이름, 고객 등급, 보너스포인트, 포인트 적립 비율
+    static int serialNums = 1; // 클래스변수
+
+    protected String customerID; // 고객id
+    protected String name; // 고객이름
+    protected String customerGrade; // 고객등급
+
+    protected int bonusPoint; // 보너스포인트
+    protected double bonusPointRatio; // 포인트 적립비율
+
+    // 행위
+    // 1. 돈을 지불한다.
+    public int calculatePrice (int price) {
+        this.bonusPoint += price * bonusPointRatio;
+        return price;
+    }
+
+   Customer(){}
+
+    // 생성자
+    Customer(String name) {
+        this.customerID = "Customer" + serialNums++;
+        // 고객Id는 Customer(숫자)로 순차생성된다.
+        this.name = name;
+        this.customerGrade = "SILVER"; // 생성시 실버 등급으로 시작한다.
+        this.bonusPointRatio = 0.01; // 생성시 포인트 비율은 0.01 포인트이다
+        this.bonusPoint = 0;
+    }
+
+    void printMyInfo() {
+        System.out.printf("Customer(customerID=%s, name=%s, customerGrade=%s, bonusPoint=%d)\n",
+                        this.customerID, this.name, this.customerGrade, this.bonusPoint);
+    }
+
+}
---
+++ JAVA/LearningJava/src/exercise/chapter_34/GolderCustomer.java
@@ -0,0 +1,29 @@
+package exercise.chapter_34;
+
+public class GolderCustomer extends Customer {
+
+    // 속성
+    private double discountRatio;
+
+    // 행위
+    // 1. 돈을 지불한다.
+    @Override
+    public int calculatePrice (int price) {
+        this.bonusPoint += price * bonusPointRatio;
+        price -= price * discountRatio;
+        return price;
+    }
+
+
+    GolderCustomer(String name){
+        super();
+        this.customerID = "Customer" + Customer.serialNums++;
+        this.name = name;
+        this.customerGrade = "GOLD";
+        this.bonusPoint = 0;
+        this.discountRatio = 0.03;
+        this.bonusPointRatio = 0.03;
+
+    }
+
+}
